c9d9425f6a53c5c741480cadbd07a10243e279d5
Kubilay <kubilay@haydesoft.com>
2021-01-09 20:15:38 +0300

    Login,Sign in,Welcome Page Design

---
+++ lib/pages/Signup/signup_screen.dart
@@ -0,0 +1,12 @@
+import 'package:artvin_sepeti/pages/Signup/components/body.dart';
+import 'package:flutter/material.dart';
+
+
+class SignUpScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
