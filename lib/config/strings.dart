8c418690e161c66988483ffe4e519be0473252f0
Kubilay <kubilay@haydesoft.com>
2021-01-11 13:44:17 +0300

    Fixed

--- lib/config/strings.dart
+++ lib/config/strings.dart
@@ -1,5 +1,4 @@
 
-
-    const String splashPageBottomText="artvin ";
+    const String splashPageBottomText="artvin";
     const String splashPageTopText="sepeti";
     const String onBoardSkipText="ATLA";
